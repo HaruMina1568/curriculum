0fcfe1e05da6aa3ba4fd6534d0b23a510a004c89
HaruMina1568 <94769943+HaruMina1568@users.noreply.github.com>
2021-12-05 18:34:13 +0900

    3章課題提出

---
+++ JavaStudy/Date_Moji_3_7/Curriculum.java
@@ -0,0 +1,11 @@
+package Date_Moji_3_7;
+public class Curriculum {
+    
+    public static void main(String[] args) {
+    
+        // int型を宣言して、「seisu」 という変数名をつけて、1234で初期化する
+        int seisu = 1234;
+        System.out.println("int型 = 整数 = " + seisu);
+    }
+
+}
---
+++ JavaStudy/Date_Moji_3_7/Curriculum2.java
@@ -0,0 +1,12 @@
+package Date_Moji_3_7;
+public class Curriculum2 {
+    
+    public static void main(String[] args) {
+        
+        // Stringクラスを宣言して、変数名はmojiで「javaはプログラム言語」で初期化
+        String moji = "javaはプログラム言語";
+
+        System.out.println(moji);
+
+    }
+}
